4d092d0618c7c0a43e2feb847f272d252a2984f0
myeongil <username1103@naver.com>
2022-09-08 21:18:26 +0900

    feat: webpack dev, prod 분리

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const path = require("path");
-const webpack = require("webpack");
-const webpackDevMiddleware = require("webpack-dev-middleware");
-const webpackConfig = require("./webpack.config.js");
+const path = require('path');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackConfig = require('./webpack.dev.js');
 const compiler = webpack(webpackConfig);
-const express = require("express");
+const express = require('express');
 const port = 3000;
 const app = express();
 
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "dist")));
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.use(
   webpackDevMiddleware(compiler, {
@@ -17,13 +17,13 @@ app.use(
   })
 );
 
-app.get("/", (req, res, next) => {
-  const filename = path.join(compiler.outputPath, "index.html");
+app.get('/', (req, res, next) => {
+  const filename = path.join(compiler.outputPath, 'index.html');
 
   compiler.outputFileSystem.readFile(filename, (err, result) => {
     if (err) return next(err);
 
-    res.set("content-type", "text/html").send(result);
+    res.set('content-type', 'text/html').send(result);
   });
 });
 
---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/public/javascripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'public', 'app.html') })],
+};
