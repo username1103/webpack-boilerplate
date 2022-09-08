47d59f70156c94e5e99b56c589cfbc578c5f0b2a
myeongil <username1103@naver.com>
2022-09-08 16:17:40 +0900

    feat: webpack, babel 적용

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /₩.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "src/app.html" })],
+};
